2030b2ff0ef6a16f4f6c45c73771aea29abf8b8c
sternsleuth <rascyber@users.noreply.github.com>
2025-04-18 10:15:53 +0200

    cleaned up source code

--- rebuild.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# rebuild.sh - recreate structure and set permissions
-
-echo "[*] Rebuilding project structure..."
-mkdir -p sharepoint-vti-bin-scanner
-mv mass-vti-bin-scanner.sh sharepoint-vti-bin-scanner/
-mv sharepoint-asmx-cves.yaml sharepoint-vti-bin-scanner/
-chmod +x sharepoint-vti-bin-scanner/mass-vti-bin-scanner.sh
-echo "[+] Done. You can now run the scanner using: ./sharepoint-vti-bin-scanner/mass-vti-bin-scanner.sh subs.txt"
